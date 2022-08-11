c1de8f8368d9d3183b0720d365601d0230bdb7a4
Daisuke Kusatake <75785995+D-ske104@users.noreply.github.com>
2022-08-11 16:51:02 +0900

    Create auto-scroller.js

---
+++ auto-scroller.js
@@ -0,0 +1,23 @@
+// ==ClosureCompiler==
+// @compilation_level SIMPLE_OPTIMIZATIONS
+// @output_file_name default.js
+// ==/ClosureCompiler==
+
+// ADD YOUR CODE HERE
+const num = window.prompt("0.05秒ごとにスクロールするpx（オススメ: 1～10）", 10)
+
+function scroller() {
+  window.scrollBy({
+    top: num,
+    behavior: "smooth"
+  })
+}
+
+let intervalId = setInterval(scroller, 50, num)
+
+function clearScroll() {
+  window.clearInterval(intervalId)
+  document.removeEventListener("wheel", clearScroll)
+}
+
+document.addEventListener("wheel", clearScroll)
